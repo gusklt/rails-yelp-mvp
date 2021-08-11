0a8ead3576e808b5839889ee6e49d93666f959e4
Augustin Clot <augustin.clot@gmail.com>
2021-08-11 15:23:49 +0200

    index route controller viex set

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new'
+  get 'restaurants/:id/show', to: 'restaurants#show'
+  get 'restaurants/review'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "+33567897353", category: "japanese" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "+237755377", category: "belgian"}
+chez_maman = { name: "Chez Maman", address: "125 boulevard Flammarion, Marseille", phone_number: "+24566677", category: "french" }
+mie_caline = { name: "Mie Caline", address: "Vieux Port, Marseille", phone_number: "+2378888377", category: "italian" }
+la_maison = { name: "La Maison", address: "Tour Eiffel, Paris", phone_number: "+22333997", category: "chinese" }
+
+[dishoom, pizza_east, chez_maman, mie_caline, la_maison].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
