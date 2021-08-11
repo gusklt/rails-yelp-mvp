f15b3bc282a1c283aaf2e0facc0f8123e3241416
Augustin Clot <augustin.clot@gmail.com>
2021-08-11 16:22:42 +0200

    retsaurant controller routes and model complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,11 +4,27 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else render :new
+    end
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def review
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new'
-  get 'restaurants/:id/show', to: 'restaurants#show'
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  post 'restaurants', to: 'restaurants#create'
   get 'restaurants/review'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
