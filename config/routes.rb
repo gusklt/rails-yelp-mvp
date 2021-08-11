caefdfbd92860ca36f04f0bf7a5c4055423b70cc
Augustin Clot <augustin.clot@gmail.com>
2021-08-11 17:08:10 +0200

    reste a developper les reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   post 'restaurants', to: 'restaurants#create'
-  get 'restaurants/review'
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
